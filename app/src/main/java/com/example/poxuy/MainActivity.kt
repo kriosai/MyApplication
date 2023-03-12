d134ad0ac65d5e3217aa62c606d3b821239b30d8
Revas <kriosai08@gmail.com>
2023-03-12 18:48:49 +0600

    init

---
+++ app/src/main/java/com/example/poxuy/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.poxuy
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val text = findViewById<TextView>(R.id.textView)
+        button.setOnClickListener { text.text = "Hello world" }
+    }
+}
